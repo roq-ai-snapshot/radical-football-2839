c7fe737095ddc6c5a56ec5b2f05c4fb7ba88613a
roq-ai <>
2023-06-02 18:39:26 +0800

    Initial commit

---
+++ src/interfaces/user/index.ts
@@ -0,0 +1,13 @@
+/* start user-interface-imports-handler */
+/* end user-interface-imports-handler */
+
+export interface UserInterface {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  roq_user_id: string;
+  tenant_id: string;
+  /* start user-interface-handler */
+  /* end user-interface-handler */
+}
